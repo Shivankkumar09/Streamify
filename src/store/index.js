4388599b963b1d5613aa707c179a92e116950d49
Shivankkumar09 <shivank0912@gmail.com>
2025-07-13 12:19:35 +0530

    .

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import {
   } from "@reduxjs/toolkit";
   import axios from "axios";
   import { API_KEY, TMDB_BASE_URL } from "../utils/constants";
-
+//store
   
   const initialState = {
     movies: [],
